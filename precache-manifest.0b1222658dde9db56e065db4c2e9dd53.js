decccfd041e48f3c1e7484c898302d349f672d05
HABEEBRAHIMANP <habeebmeladi@gmail.com>
2021-01-26 17:18:05 +0530

    Updates

---
+++ precache-manifest.0b1222658dde9db56e065db4c2e9dd53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "739fcd8fcfd2f34f8c32d0de564f76b7",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "62ec531e2ddd2c177862",
+    "url": "/react/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/react/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/react/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62ec531e2ddd2c177862",
+    "url": "/react/static/js/main.a64e0515.chunk.js"
+  },
+  {
+    "revision": "95cd91dfdf87c0a2be75",
+    "url": "/react/static/js/runtime-main.710dcc97.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/react/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "3e26bc28f382469340f8066023744265",
+    "url": "/react/static/media/resume.3e26bc28.pdf"
+  }
+]);
